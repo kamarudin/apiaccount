202931c9e10bc14c35425363aa0cbd08d4b9ad8f
Kamarudin Samsudin <kamarudin@gmail.com>
2020-09-08 10:42:45 +0800

    add "ping" in routes

--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::get('ping', 'ApiController@getPing');
 
 Route::get('accounts', 'ApiController@getAllAccounts');
 Route::get('accounts/{id}', 'ApiController@getAccount');
